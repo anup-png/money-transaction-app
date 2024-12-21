f7edef1bfc3bff0a0cc5acdb30bbcecc757ba70b
anup-coding <113417001+anup-png@users.noreply.github.com>
2024-12-21 14:15:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use("/api/v1",mainRouter); //any request coming at /api/v1 will be redirecte
 app.listen(3000,()=>{
     console.log("Server is running at port 3000");
 });
-// mongoose.connect("mongodb+srv://anupRaj:54w742WRw2U36Qkv@cluster0.maqxzmb.mongodb.net/myPaytm");
+
 
 
 
